8bef424951d356f4d1823c7ff3298c20e4d61776
James Dunham <james@jamesdunham.io>
2016-10-23 12:29:44 -0400

    check for valid id in design()

--- R/design.r
+++ R/design.r
@@ -8,8 +8,10 @@
 #' @return By default, a list representing the survey design; see \code{as}.
 #' @seealso Download a survey's \code{\link{responses}},
 #'   \code{\link{questions}}, or question \code{\link{choices}}.
+#' @import assertthat
 #' @export
 design = function(id, as = "parsed") {
+  assertthat::assert_that(assertthat::is.string(id))
   r = qget(action = paste0("surveys/", id), as = as)
   if (identical(as, "parsed")) {
     return(r$result)
