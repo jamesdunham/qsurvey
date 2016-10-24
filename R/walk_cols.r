bd4c9116509981055b2fa4212331803236bb760a
James Dunham <james@jamesdunham.io>
2016-10-24 19:02:46 -0400

    WIP on walk_cols

--- R/walk_cols.r
+++ R/walk_cols.r
@@ -1,33 +1,45 @@
 function() {
-id = "SV_bKQGl5WB66K9yFT"
-r
-sd = design(id)
-head(sd)
-tail(sd)
-head(sq)
+  key_from_file()
+  id = "SV_bKQGl5WB66K9yFT"
 
-r = responses(id)
-stopifnot(nrow(r))
-sq = questions(id)
-stopifnot(all(sq$export_label %in% names(r)))
+  sd = design(id)
+  r = responses(id, use_labels = FALSE)
+  r[, grepl('fraud', names(r)), with = FALSE]
+  r[, grepl('confidence', names(r)), with = FALSE]
+  r = responses(id, use_labels = TRUE)
+  r[, grepl('fraud', names(r)), with = FALSE]
+  r[, grepl('confidence', names(r)), with = FALSE]
+  r[, grepl('husted', names(r)), with = FALSE]
 
-x[1:5, 1:5]
-x = cbind(as.matrix(names(r)), matrix(NA, ncol = length(names(r)),
-  nrow = length(names(r)), dimnames = list(c(names(r)), names(r))))
-names(x)
-x[1:5, 1:5]
+  prop.table(table(r[, fraud_statement_attribution], useNA = 'always'))
+  prop.table(table(r[, husted_intro], useNA = 'always'))
+  names(r)
+  stopifnot(nrow(r) > 0)
+  qu = questions(id)
+  str(qu)
+  ch = choices(id)
+  en = export_names(id)
+  en[grepl('fraud', export_name)]
+  ch[question %in% c("QID20", "QID21")]
 
-# there are 45 export labels
-y = lapply(sq$export_label, function(q) {
-  # if variable q isn't missing, how often is (each other) variable p
-  # non-missing?
-  res = r[!is.na(get(q)) & get(q) != "", lapply(.SD, function(p)
-    sum(!is.na(p) & p != "")), .SDcols = sq$export_label]
-  res[, varname := q][]
-  })
-z = data.table::rbindlist(y)
-data.table::setcolorder(z, c("varname", setdiff(names(z), "varname")))
-dim(z)
-z[1:5, 1:5, with = F]
+  sq = questions(id)
+  sq[, id := as.numeric(sub("QID", "", question))]
+  data.table::setkeyv(sq, "id")
 
+  stopifnot(all(sq$export_label %in% names(r)))
+
+  # there are 45 export labels
+  names(r)
+  cols = names(r)
+  y = lapply(cols, function(q) {
+    # if variable q isn't missing, how often is (each other) variable p
+    # non-missing?
+    res = r[!is.na(get(q)) & get(q) != "", lapply(.SD, function(p)
+      sum(!is.na(p) & p != "")), .SDcols = cols]
+    res[, lapply(.SD, function(p) round(p / .SD[[q]], 2))]
+    })
+  y = setNames(y, cols)
+  z = data.table::rbindlist(y, idcol = "varname")
+  dim(z)
+  z[1:5, 1:5, with = F]
 }
