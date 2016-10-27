6fb20dfbdc4b2e475f6569239f52e029fc713427
James Dunham <james@jamesdunham.io>
2016-10-26 23:28:28 -0400

    rm ... from surveys sig

--- R/surveys.r
+++ R/surveys.r
@@ -4,13 +4,11 @@
 #' The result is sorted by the \code{lastModified} timestamp in descending
 #' order.
 #'
-#' @param ... Further arguments to \code{\link{request}}.
-#'
 #' @return A data.table of survey metadata
 #' @importFrom lubridate ymd_hms
 #' @export
-surveys = function(...) {
-  surveys = get_pages("surveys", ...)
+surveys = function() {
+  surveys = get_pages("surveys")
   surveys = Reduce(c, surveys)
   tbl = data.table::rbindlist(surveys)
   data.table::setkeyv(tbl, "name")
@@ -19,13 +17,13 @@ surveys = function(...) {
   return(tbl[])
 }
 
-get_pages = function(page, ...) {
+get_pages = function(page) {
   # The result of a request to the surveys API can span multiple pages. Get the
   # first page of the result, and then request the next page of results, until
   # there are no more pages.
   res = list()
   while (length(page)) {
-  parsed = qget(action = page, ...)
+  parsed = qget(action = page)
     if (length(parsed$result$nextPage)) {
       page = paste0(page, "?", sub(".*\\?", "", parsed$result$nextPage))
     } else {
