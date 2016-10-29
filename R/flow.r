7bb53d8a8100cb27d39a327350eee7219528fea6
James Dunham <james@jamesdunham.io>
2016-10-29 17:26:06 -0400

    add plotting via visNetwork

--- R/flow.r
+++ R/flow.r
@@ -1,65 +1,63 @@
 utils::globalVariables(c("edges", "br_subset", "edge_type", "edge_color", "name",
-  "node", "parent", "width", "previous", "node_color", "node_shape"))
+  "node", "parent_id", "width", "from", "node_color", "node_shape"))
 
-#' Represent a survey design as a graph
-#'
-#' \code{plot_flow} and \code{edges} represent a survey's design as a directed
-#' graph that shows possible paths through the elements of the survey.
+#' Represent survey flows as node or edge tables
 #'
 #' @inheritParams choices
-#' @return \code{plot_flow} invisibly returns a DiagrammeR \code{dgr_graph}
-#'   object. As a side effect, it renders the graph. \code{edges} returns a
-#'   data.table of graph edges.
-#'
-#' @import DiagrammeR
-#' @aliases edges
-#' @export
-plot_flow = function(design) {
-  assertthat::assert_that("qualtrics_design" %in% class(design))
-  edge_tbl = search_flow(design$flow)
-  edge_tbl = add_edge_types(edge_tbl)
-  edge_tbl = add_edge_colors(edge_tbl)
-  edge_tbl = add_node_types(edge_tbl)
-  edge_tbl = add_node_colors(edge_tbl)
-  edge_tbl = add_node_shapes(edge_tbl)
-  edge_tbl = add_block_descriptions(edge_tbl, design)
-  nodes = DiagrammeR::create_nodes(
-    nodes = c(0, edge_tbl$node),
-    label = c("Start", edge_tbl$node_type),
-    type = c("Start", edge_tbl$node_type),
-    shape = c("circle", edge_tbl$node_shape),
-    width = c(0.4, edge_tbl$width),
-    fillcolor = c("#fef4ab", edge_tbl$node_color))
-  edge_tbl = DiagrammeR::create_edges(
-    from = edge_tbl$previous,
-    to = edge_tbl$node,
-    color = edge_tbl$edge_color)
-
-  graph = DiagrammeR::create_graph(
-    nodes_df = nodes,
-    edges_df = edge_tbl,
-    graph_attrs = "layout = dot; rankdir = LR")
-  graph = DiagrammeR::set_node_attrs(graph, "fontcolor", "black")
-  DiagrammeR::render_graph(graph)
-  invisible(graph)
-}
-
-#' @name plot_flow
-#' @inheritParams plot_flow
+#' @aliases nodes
 #' @export
 edges = function(design) {
   assertthat::assert_that("qualtrics_design" %in% class(design))
   edge_tbl = search_flow(design$flow)
+  edge_tbl = add_edge_types(edge_tbl)
+  edge_tbl[, c("node_type", "label", "parent_id") := NULL, with = FALSE]
+  data.table::setnames(edge_tbl, c("id", "edge_type"), c("to", "type"))
+  data.table::setcolorder(edge_tbl, c("from", setdiff(names(edge_tbl), "from")))
   edge_tbl[]
+}
+
+#' @rdname edges
+#' @export
+nodes = function(design, ids = FALSE) {
+  assertthat::assert_that("qualtrics_design" %in% class(design))
+  node_tbl = search_flow(design$flow)
+  data.table::setnames(node_tbl, c("node_type"), c("type"))
+  node_tbl[, c("from") := NULL, with = FALSE]
+
+  # the parent_id of level-1 nodes is 0; create a node with id 0 to represent
+  # the start of the survey, though no sucha type exists in the Qualtrics API
+  # node_tbl = data.table::rbindlist(list(data.frame(id = 0, type = "StartSurvey"),
+    # node_tbl), fill = TRUE)
+
+  # add descriptive labels for blocks
+  block_tbl = blocks(design)
+  node_tbl = merge(node_tbl, block_tbl, all.x = TRUE, all.y = FALSE,
+    by.x = "label", by.y = "id")
+  # use descriptions instead of BL_xx ids in block labels
+  if (isTRUE(ids)) {
+    node_tbl[type == "Block", block_id := label]
+  }
+  node_tbl[type == "Block", label := description]
+  node_tbl[, description := NULL]
+  data.table::setcolorder(node_tbl, union(c("id", "parent_id", "type",
+    "label"), names(node_tbl)))
+
+  node_tbl[type == "StartSurvey", label := "Start"]
+  node_tbl[type == "EndSurvey", label := "End"]
+  node_tbl[type == "EmbeddedData", label := "Set Data"]
+  # finally use types in place of missing labels
+  node_tbl[is.na(label), label := type]
+
+  node_tbl[]
 }
 
 search_flow = function(f) {
   nodes = f[sapply(f, is.list)]
   edges = list(
-    "node" = numeric(),
-    "parent" = numeric(),
-    "name" = character(),
-    "br_subset" = numeric()
+    "id" = numeric(),
+    "parent_id" = numeric(),
+    "node_type" = character(),
+    "label" = character()
   )
   n = 0
   parent_n = 0
@@ -68,20 +66,22 @@ search_flow = function(f) {
     n = n + 1
     parent_n = ifelse(length(names(nodes[1])) && names(nodes[1]) != "",
       as.numeric(names(nodes[1])), parent_n)
-    br_options = NULL
+    node_label = NA
+    node_flow = NA
     # nodes is a FIFO queue
     node_type = nodes[[1]]$type
     if (node_type == "Block") {
-      node_type = paste(node_type, nodes[[1]]$id)
+      node_label = nodes[[1]]$id
       nodes[1] = NULL
     } else if (node_type %in% c("Branch", "BlockRandomizer")) {
       node_flow = nodes[[1]]$flow
       nodes = c(nodes, setNames(node_flow, rep(n, length(node_flow))))
       if (node_type == "BlockRandomizer") {
-        br_options = list(
+        node_label = paste0("Randomize: ",
           nodes[[1]]$randomizationOptions$randomSubset,
-          nodes[[1]]$randomizationOptions$evenPresentation
-        )
+          " of ", length(node_flow),
+          ifelse(nodes[[1]]$randomizationOptions$evenPresentation,
+            " (evenly)", ""))
       }
       nodes[1] = NULL
     } else {
@@ -90,70 +90,60 @@ search_flow = function(f) {
     edges = data.table::rbindlist(list(
       edges,
       data.frame(
-        "node" = n,
-        "parent" = parent_n,
-        "name" = as.character(node_type),
-        "br_subset" = ifelse(is.null(br_options), NA, br_options[[1]]),
-        "br_presentation" = ifelse(is.null(br_options), NA, br_options[[2]]),
+        "id" = n,
+        "parent_id" = parent_n,
+        "node_type" = node_type,
+        "label" = node_label,
         stringsAsFactors = FALSE
       )),
       fill = TRUE
     )
-    message(parent_n, " - ", n, " (", node_type, ")")
   }
   edges = order_siblings(edges)
   edges[]
 }
 
 order_siblings = function(edge_tbl) {
-  not_randomizers = unique(edge_tbl[name != "BlockRandomizer", node])
-  edge_tbl[parent %in% c(0, not_randomizers), previous := data.table::shift(node),
-    by = "parent"]
-  edge_tbl[is.na(previous), previous := parent]
+  not_randomizers = unique(edge_tbl[node_type != "BlockRandomizer", id])
+  edge_tbl[parent_id %in% c(0, not_randomizers), from := data.table::shift(id),
+    by = "parent_id"]
+  edge_tbl[is.na(from), from := parent_id]
   edge_tbl[]
 }
 
 add_edge_types = function(edge_tbl) {
-  is_branch = unique(edge_tbl[name == "Branch", node])
-  edge_tbl[parent %in% is_branch, `:=`(edge_type = "conditional")]
-  is_randomizer = unique(edge_tbl[name == "BlockRandomizer", node])
-  edge_tbl[parent %in% is_randomizer, `:=`(edge_type = "random")]
+  is_branch = unique(edge_tbl[node_type == "Branch", id])
+  edge_tbl[parent_id %in% is_branch, `:=`(edge_type = "conditional")]
+  is_randomizer = unique(edge_tbl[node_type == "BlockRandomizer", id])
+  edge_tbl[parent_id %in% is_randomizer, `:=`(edge_type = "random")]
   edge_tbl[is.na(edge_type), `:=`(edge_type = "deterministic")]
   edge_tbl[]
 }
 
-add_node_types = function(edge_tbl) {
-  edge_tbl[grepl("^Block BL_", name), node_type := "Block"]
-  edge_tbl[grepl("^EmbeddedData$", name), node_type := "Data"]
-  edge_tbl[grepl("^WebService$", name), node_type := "Web Svc."]
-  edge_tbl[grepl("^Authenticator$", name), node_type := "Auth."]
-  edge_tbl[grepl("^TableOfContents$", name), node_type := "TOC"]
-  edge_tbl[grepl("^BlockRandomizer$", name), node_type := "Rand."]
-  edge_tbl[grepl("^EndSurvey$", name), node_type := "End"]
-  edge_tbl[is.na(node_type), node_type := name]
-  edge_tbl[]
-}
-
 add_node_shapes = function(edge_tbl) {
-  edge_tbl[node_type == "Block", node_shape := "square"]
-  edge_tbl[node_type == "Block", width := 1.1]
-  edge_tbl[node_type == "End", node_shape := "circle"]
-  edge_tbl[is.na(node_shape), node_shape := "circle"]
+  edge_tbl[type == "EmbeddedData", shape := "dot"]
+  edge_tbl[type == "BlockRandomizer", shape := "dot"]
+  edge_tbl[type == "Branch", shape := "dot"]
+  edge_tbl[type == "Block", shape := "dot"]
+  # StartSurvey isn't a flow element type in the Qualtrics API, but is used in
+  # qsurvey to represent the start of the survey
+  edge_tbl[type %in% c("StartSurvey", "EndSurvey"), shape := "dot"]
+  edge_tbl[is.na(shape), shape := "dot"]
   edge_tbl[]
 }
 
 add_node_colors = function(edge_tbl) {
-  edge_tbl[node_type == "End", node_color := "#fef4ab"]
-  edge_tbl[node_type == "Block", node_color := "#d9d9d9"]
-  edge_tbl[node_type == "Branch", node_color := "#fc9272"]
-  edge_tbl[node_type == "Rand.", node_color := "#fc9272"]
-  edge_tbl[node_type == "Data", node_color := "#a3c4cd"]
+  edge_tbl[type %in% c("StartSurvey", "EndSurvey"), color := "#fef4ab"]
+  edge_tbl[type == "Block", color := "#d9d9d9"]
+  edge_tbl[type == "Branch", color := "#fc9272"]
+  edge_tbl[type == "BlockRandomizer", color := "#fc9272"]
+  edge_tbl[type == "EmbeddedData", color := "#a3c4cd"]
   edge_tbl[]
 }
 
 add_edge_colors = function(edge_tbl) {
-  edge_tbl[edge_type == "deterministic", edge_color :=  "#000000"]
-  edge_tbl[edge_type %in% c("conditional", "orange"), edge_color :=  "orange"]
+  edge_tbl[type == "deterministic", color :=  "#000000"]
+  edge_tbl[type %in% c("conditional", "random"), color :=  "orange"]
   edge_tbl[]
 }
 
--- R/plot.r
+++ R/plot.r
@@ -1,1 +1,31 @@
-# Plot survey flow
+#' Represent a survey design as a graph
+#'
+#' \code{plot_flow} and \code{edges} represent a survey's design as a directed
+#' graph that shows possible paths through the elements of the survey.
+#'
+#' @inheritParams choices
+#' @return \code{plot_flow} invisibly returns a DiagrammeR \code{dgr_graph}
+#'   object. As a side effect, it renders the graph. \code{edges} returns a
+#'   data.table of graph edges.
+#'
+#' @import visNetwork
+#' @aliases edges
+#' @export
+plot_flow = function(design) {
+  edge_tbl = edges(design)
+  edge_tbl = add_edge_colors(edge_tbl)
+
+  node_tbl = nodes(design)
+  node_tbl = add_node_colors(node_tbl)
+
+  net = visNetwork::visNetwork(node_tbl, edge_tbl)
+  net = visNetwork::visInteraction(net, dragNodes = FALSE, dragView = FALSE, zoomView = FALSE)
+  net = visNetwork::visEdges(net, arrows = "to")
+  net = visNetwork::visNodes(net, shape = "dot")
+  # net = visNetwork::visLegend(net, useGroups = FALSE, addEdges =
+      # data.frame(label = c("deterministic", "conditional"), color = c("black", "orange")),
+    # position = "right", main = "Edges")
+  visNetwork::visHierarchicalLayout(net, direction = "LR", sortMethod = "directed")
+
+  invisible(net)
+}
